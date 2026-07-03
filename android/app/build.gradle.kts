ac5f01ffa5a7f4f126d2372c4012907a04758004
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-07-03 16:13:18 +0300

    solving issues as i try to run flutter

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.aflalert.aflalert"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
