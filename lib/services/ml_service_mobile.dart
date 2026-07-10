557d0a6a502248f1cb4e72284cb53bb290dec0e7
lancer-freeman <kizitojumalancer@gmail.com>
2026-07-10 17:11:20 +0300

    updates

--- lib/services/ml_service_mobile.dart
+++ lib/services/ml_service_mobile.dart
@@ -1,10 +1,10 @@
 // ml_service_mobile.dart
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'ml_service.dart';
-
+import 'tflite_service.dart';
 class MobileMlService implements MyMlService {
   late Interpreter _interpreter;
-
+  final TfliteService _tfliteService = TfliteService();
   @override
   void initialize() async {
     _interpreter = await Interpreter.fromAsset('model.tflite');
@@ -12,6 +12,9 @@ class MobileMlService implements MyMlService {
 
   @override
   void predict(dynamic input) {
+  }
+
+  @override
   Future<Map<String, dynamic>?> classifyMaize(String filePath) async {
     
     return await _tfliteService.classifyMaize(filePath);
