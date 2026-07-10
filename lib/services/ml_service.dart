9526778b6c8908755d2c955a9642e15a6890bba7
lancer-freeman <kizitojumalancer@gmail.com>
2026-07-10 17:53:04 +0300

    updates

--- lib/services/ml_service.dart
+++ lib/services/ml_service.dart
@@ -1,10 +1,13 @@
 // ml_service.dart
 
 abstract class MyMlService {
+ 
   void initialize();
   void predict(dynamic input);
   Future<Map<String, dynamic>?> classifyMaize(String filePath);
 }
+
+
 
 // This factory trick dynamically swaps out the implementation at runtime
 MyMlService getMlService() => throw UnsupportedError('Cannot create service');
