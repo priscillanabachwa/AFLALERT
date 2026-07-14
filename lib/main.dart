b2cc702e2b7c035ce0af1313c5c8df02ab9f8d19
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-07-14 10:28:49 +0300

    merge issues

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:aflalert/screens/downloaded_reports_screen.dart';
 import 'package:aflalert/screens/settings_screen.dart';
 import 'package:aflalert/screens/result_screen.dart';
 import 'package:aflalert/services/local_notification_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,14 +29,38 @@ Future<void> main() async {
   runApp(const AflAlert());
 }
 
-class AflAlert extends StatelessWidget {
+class AflAlert extends StatefulWidget {
   const AflAlert({super.key});
 
+  @override
+  State<AflAlert> createState() => _AflAlertState();
+}
+
+class _AflAlertState extends State<AflAlert> {
+  ThemeMode _themeMode = ThemeMode.light;
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _themeMode = (prefs.getBool('darkModeEnabled') ?? false)
+          ? ThemeMode.dark
+          : ThemeMode.light;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      themeMode: _themeMode,
+
+  
+       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFF00462D),
@@ -98,6 +123,18 @@ class AflAlert extends StatelessWidget {
           ),
         ),
       ),
+       darkTheme: ThemeData(
+    brightness: Brightness.dark,
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF00462D),
+      brightness: Brightness.dark,
+    ),
+    textTheme: GoogleFonts.poppinsTextTheme(
+      ThemeData.dark().textTheme,
+    ),
+  ),
+
 
       // First screen shown when the app starts (serves as '/')
       home: const SplashScreen(),
@@ -117,7 +154,6 @@ class AflAlert extends StatelessWidget {
             isSafe: args.isSafe,
             confidence: args.confidence,
             analysisLabel: args.analysisLabel,
-            imagePath: args.imagePath,
           );
         },
         '/register': (context) => const RegistrationScreen(),
