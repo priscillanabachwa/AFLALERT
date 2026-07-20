ea446116b010a66ad91d7ccc79e2f12491c66439
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-07-20 18:02:11 +0300

    removed ss.

--- lib/screens/analysis_screen.dart
+++ lib/screens/analysis_screen.dart
@@ -324,7 +324,7 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: bullets
             .map(
-              (bulsslet) => Padding(
+              (bullet) => Padding(
                 padding: const EdgeInsets.only(bottom: 14),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
