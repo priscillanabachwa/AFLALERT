a303b24f74fd3be38a89ac25db3ebb9f12261610
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-07-22 17:26:04 +0300

    resloved merge conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Platform;
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
